292f0c634fb318b0b9c7a459b830f32f685c9356
Florent <grenaille.florent85@gmail.com>
2018-04-30 10:48:41 +0200

    ajout de contenue dans l'aide mémoire Python

---
+++ A_M_Python.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+"""
+Title: Aide Mémoire pour python
+Date: 19/03/2018
+Create by: Florent
+"""
+
+print("Types Standards: ")
+print("\tentier numérique (int)")
+print("\tnombre flottant (float)")
+print("\tchaîne de caractères (str)")
+print("\tbooléen (bool)")
+print(" ")
+print("\tprint() -> affiche a l'écran")
+print("\tinput() -> lire les entrées clavier")
+print("\ttype() -> retourne le type d'une donnée, variable, etc...")
+print("\tint(), float(), str(), bool() -> caster, convertir une donnée, varaible...")
