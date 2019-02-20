6f2251dcac5f5dffe9bb728cbc87808ee0995d08
Florent <grenaille.florent85@gmail.com>
2019-02-20 18:43:30 +0100

    complétion de A_M_SQL.sql

--- A_M_Sql.sql
+++ A_M_Sql.sql
@@ -31,7 +31,10 @@ plusieurs lignes*/
         'id_user' INT,
         'user_name' VARCHAR(65),
         'user_birthday' DATE
-    );
+    )
+    ENGINE = MyISAM --> Pour spécifier le moteur de stockage
+    CHARACTER SET utf8md4 --> Pour spécifier le type d'encodage
+    COLLATE utf8mb4_unicode_ci; --> Pour spécificer le type insenssible à la case
     
 -- Afficher les informations d'une table:
     DESCRIBE 't_users'; -- DESCRIBE/DESC/EXPLAIN = même résultat
@@ -49,6 +52,19 @@ plusieurs lignes*/
     DROP TABLE 't_users';
     -- Ou:
     DROP TABLE IF EXISTS 't_users';
+    
+-- Contraintes:
+    NOT NULL                    --> Champs(colonne) obligatoire (ex: 'id_user' SMALLINT(6) NOT NULL, )
+    DEFAULT                     --> Champs(colonne) avec une valeurs par défaut (ex:: 'user_admin' BOOLEAN DEFAULT 0 )
+    CHECK(condition a test)     --> Champs(colonne) avec une vérification de condition (ex: 'id_user' SMALLINT(6) NOT NULL CHECK(id_user <= 10000, )
+    UNIQUE(valeurUnique)        --> Valeur de champ unique. Pour un site web par exemple, les identifiants de compte son unique (ex: UNIQUE('user_name') )
+    PRIMARY KEY('colonne')      --> Clé primaire. C'est le système qui gère les id de cette colonne (ex: PRIMARY KEY('id_user') )
+    AUTO_INCREMENT              --> Incrémentation automatique (ex: 'id_user' SMALLINT(6) NOT NULL AUTO_INCREMENT,)
+    FOREIGN KEY('colonne') REFERENCES 'table_references'('colonne') --> Clé étrangère, pour faire une relation avec une autre table
+    
+    CREATE INDEX 'idx_users' ON 'table_users'('user_name', 'user_birthday'); --> création d'un index pour améloirer la recherche lors d'une requête
+    
+
     
 -- Pour arrêter d'utiliser une BDD et revenir au début:
     USE MYSQL;
