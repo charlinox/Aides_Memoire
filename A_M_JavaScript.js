35fae7dcf91443812d6099a6d8f0eb0b02ff2b38
Florent <grenaille.florent85@gmail.com>
2019-07-18 11:11:55 +0200

    Modification de 'A_M_Html.html' et de 'A_M_JavaScript.js'

--- A_M_JavaScript.js
+++ A_M_JavaScript.js
@@ -1,5 +1,14 @@
 /* Fichier lier avec du code HTML */
 
 function affiche() {
-    alert('Bonjour le monde');
+    alert('Bonjour le monde'); // affiche un message dans une boite de dialogue
+    console.log("Bonjour le monde"); // affiche un message dans la console de log des navigateurs
 }
+
+// DOM (Documents Objects Models)
+
+var mainContent = document.getElementById("mainContent");   // récupere les infos DOM de l'objet qui a comme id "mainContent"
+console.log(mainContent);
+mainContent.setAttribute("align", "right");     // ajoute l'attribut "align" et la valeur "right" a l'objet qui a comme id "mainContent" (peut etre fais en CSS aussi)
+
+var mainTitle = document.getElementById("mainTitle");
